09e2ee1f4b09539644d65fe8b8530cc21030c231
KendleMintJed <KendleMintJed@proton.me>
2025-08-14 23:41:50 +0100

    Make basic working message popover button

---
+++ index.tsx
@@ -0,0 +1,74 @@
+import { ChatBarButton, ChatBarButtonFactory } from "@api/ChatButtons";
+import { definePluginSettings } from "@api/Settings"
+import definePlugin, { OptionType } from "@utils/types";
+import { ChannelStore, UserStore } from "@webpack/common";
+import { sendBotMessage } from "@api/Commands";
+
+const PopOverIcon = () => {
+    return <svg
+        fill="var(--header-secondary)"
+        width={24} height={24}
+        viewBox={"0 0 64 64"}
+    >
+        <path d="M 32 9 C 24.832 9 19 14.832 19 22 L 19 27.347656 C 16.670659 28.171862 15 30.388126 15 33 L 15 49 C 15 52.314 17.686 55 21 55 L 43 55 C 46.314 55 49 52.314 49 49 L 49 33 C 49 30.388126 47.329341 28.171862 45 27.347656 L 45 22 C 45 14.832 39.168 9 32 9 z M 32 13 C 36.963 13 41 17.038 41 22 L 41 27 L 23 27 L 23 22 C 23 17.038 27.037 13 32 13 z" />
+    </svg> ;
+}
+/*
+const ChatBarIcon: ChatBarButtonFactory = ({ isMainChat }) => {
+    if (!isMainChat) return null;
+
+    return (
+        <ChatBarButton
+            tooltip="Encrypt Message"
+            onClick={() => buildEncModal()}
+
+            buttonProps={{
+                "aria-haspopup": "dialog",
+            }}
+        >
+            <svg
+                aria-hidden
+                role="img"
+                width="20"
+                height="20"
+                viewBox={"0 0 64 64"}
+                style={{ scale: "1.39", translate: "0 -1px" }}
+            >
+                <path fill="currentColor" d="M 32 9 C 24.832 9 19 14.832 19 22 L 19 27.347656 C 16.670659 28.171862 15 30.388126 15 33 L 15 49 C 15 52.314 17.686 55 21 55 L 43 55 C 46.314 55 49 52.314 49 49 L 49 33 C 49 30.388126 47.329341 28.171862 45 27.347656 L 45 22 C 45 14.832 39.168 9 32 9 z M 32 13 C 36.963 13 41 17.038 41 22 L 41 27 L 23 27 L 23 22 C 23 17.038 27.037 13 32 13 z" />
+            </svg>
+        </ChatBarButton>
+    );
+};
+*/
+const settings = definePluginSettings({
+    defaultPassword: {
+        type: OptionType.STRING,
+        default: "",
+        description: "The default encryption password."
+    }
+})
+
+export default definePlugin({
+    name: "Encrypt",
+    description: "Symetrically encrypt/decrypt text in Discord!",
+    authors: [{name: "KendleMintJed", id: 308673958730006529n}],
+    settings,
+
+    renderMessagePopoverButton(message) {
+        return {
+            label: "Decrypt Message",
+            icon: PopOverIcon,
+            message: message,
+            channel: ChannelStore.getChannel(message.channel_id),
+            onClick: async () => {
+                sendBotMessage(
+                    message.channel_id,
+                    {
+                        content: message?.content,
+                        author: UserStore.getCurrentUser(),
+                    }
+                )
+            }
+        }
+    },
+})
